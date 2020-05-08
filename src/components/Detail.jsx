e59261ed0fb4ba158aed3500485d22440da68bae
Sunah KIM <kosah302@gmail.com>
2020-05-08 11:57:54 +0200

    Add detil info

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -9,13 +9,41 @@ const Detail = ({ place, loading, match, getPlace }) => {
         // eslint-diable-next-line
     }, [])
    
+    const imageStyle = {
+        width : '50%'
+    }
+
+    const detailStyle = {
+        marginTop: '20px',
+        width: '80vw',
+        height: '80vh',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+        // background: $accent-color
+    }
+
+    const textStyle = {
+        color: 'white',
+        margin: '2vw',
+        fontSize: '3vw'
+    }
+
     if (!place)  return <Spinner />;
     else {
         console.log("place info ", place);
         return (
-            <Fragment>
-                <h2>{place.id}</h2>
-            </Fragment>
+            <div className='detail-container' style={detailStyle}>
+                <Fragment>
+                    <img src={place.picture} alt="place-main-img" style={imageStyle}/>
+                    <div class="details-info" style={textStyle}>
+                        <p href={place.schedule.en || place.schedule.fr}></p>
+                        <p> {place.phone} </p>
+                        <p> {place.address}</p>
+                        <p> {place.website}</p>
+                        <p> Share </p>
+                    </div>
+                </Fragment>
+            </div>
         );
     }
 }
