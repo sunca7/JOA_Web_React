9001a38ce8fbeacddd124c7860ef167439e8438e
Sunah KIM <kosah302@gmail.com>
2020-06-08 22:04:10 +0200

    Add share icons

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -4,6 +4,21 @@ import { Slide } from 'react-slideshow-image';
 import Spinner from './layout/Spinner';
 import JoaContext from '../context/joa/joaContext';
 import DetailMap from './DetailMap'
+import {
+    EmailShareButton,
+    FacebookShareButton,
+    RedditShareButton,
+    TwitterShareButton,
+    WhatsappShareButton,
+  } from "react-share";
+
+  import {
+    EmailIcon,
+    FacebookIcon,
+    RedditIcon,
+    TwitterIcon,
+    WhatsappIcon,
+  } from "react-share";
 
 const Detail = ({ match }) => {
     const joaContext = useContext(JoaContext);
@@ -44,7 +59,12 @@ const Detail = ({ match }) => {
                 {selectedPlace.phone && <p> <i className="fas fa-phone-square-alt"/> {selectedPlace.phone}  </p>}
                 {selectedPlace.address && <p> <i className="fas fa-map-marked-alt"/> {selectedPlace.address} </p>}
                 {selectedPlace.website && <a target="_blank" rel="noopener noreferrer" href={selectedPlace.website}> <p> <i className="fas fa-home"/> <text>{selectedPlace.website}</text></p></a>}
-                <p> <i className="fas fa-share-alt"/> Share </p>
+                <p> <i className="fas fa-share-alt share"/> 
+                <EmailShareButton url="www.google.com"><EmailIcon round={true} /></EmailShareButton>
+                <FacebookShareButton url="www.google.com" ><FacebookIcon id='icon' round={true} /></FacebookShareButton>
+                <RedditShareButton url="www.google.com" ><RedditIcon id='icon' round={true} /></RedditShareButton>
+                <TwitterShareButton url="www.google.com"><TwitterIcon id='icon' round={true} /></TwitterShareButton>
+                <WhatsappShareButton url="www.google.com"> <WhatsappIcon id='icon' round={true} /></WhatsappShareButton></p>
             </div>
                 <DetailMap />
         </div>
